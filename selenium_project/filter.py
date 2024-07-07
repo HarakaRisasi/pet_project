e72347a1dae3244fee1f0bad29b6ed1d600c9db2
HarakaRisasi <harakarisasi@gmail.com>
2024-07-07 21:17:20 +0700

    new commit

--- selenium_project/filter.py
+++ selenium_project/filter.py
@@ -1,33 +1,80 @@
+import pandas as pd
 import os
-import re
+import datetime as datetime
 
-os.system('cls' if os.name == 'nt' else 'clear')
+dataframe = pd.read_csv(
+    'C:\\master_run\\data_history\\marketplaces\\wildberries\\sent_to\\2024-02-03\\df_final_stock.csv')
+dataframe = dataframe.drop_duplicates(subset=['offer_id', 'warehouse_id']).reset_index(drop=True)
+dataframe
 
-
-####################> Filters <####################
-def recipient_of_numbers(text_file):
-    '''remove newline in text and get only numbers ['01', '02', ...]'''
-    clean = open(f'{text_file}.txt', encoding='utf-8').read().replace('\n', '')
-    route = re.findall(r"\d{2}", clean)
-    return route
+history_dir = 'C://master_run//data_history//marketplaces//wildberries//sent_to//'
+output_file = 'df_final_stock.csv'
 
 
-def recipient_of_main_path_numbers(text_file):
-    '''finds in the text and returns a sequence of the form '11.11.11 -' consisting of one two or three pairs of numbers'''
-    clean = open(f'{text_file}.txt', encoding='utf-8').read()
-    route = re.findall(r"\d*.\d*.\d* −", clean)
-    return route
+def save_to_csv(dataframe, history_dir, output_file, result_dir='', is_only_history=True):
+    """Сохранение датафрейма в указанную дирректорию маркетплейса
+
+    Аргументы:
+        dataframe (pd.DataFrame): датафрейм, который требуется сохранить
+        output_dir (str): дирректория в которую сохраняем исторические данные
+        output_file (str): название файла
+
+    Примечания:
+        - Если дирректория с указанным маркетплейсом отсутствует,
+            то она создается
+        - Если дирректория с текущей датой отсутствует, то она создается
+        - Ежедневно создается одна директория с текущей датой,
+            в которую помещаются исторические записи по определенному
+            мпркетплейсу
+    """
+    logger.info(f"Начинаю сохранять данные в файл {output_file}")
+
+    current_date = datetime.now().strftime("%Y-%m-%d")
+    directory = f"{history_dir}{current_date}/"
+
+    is_dir_mp = os.path.isdir(result_dir)
+    if not is_only_history:
+        if not is_dir_mp:
+            logger.debug(f"Создаю дирректорию {result_dir}")
+            os.makedirs(result_dir)
+
+        is_dir_mp = os.path.isdir(history_dir)
+        if not is_dir_mp:
+            logger.debug(f"Создаю дирректорию {history_dir}")
+            os.makedirs(history_dir)
+
+        is_dir = os.path.isdir(directory)
+        if not is_dir:
+            logger.debug(f"Создаю дирректорию {directory}")
+            os.makedirs(directory)
+
+        dataframe.to_csv(f"{directory}{output_file}", index=False)
+        dataframe.to_csv(f"{result_dir}{output_file}", index=False)
+
+    else:
+        is_dir = os.path.isdir(directory)
+        if not is_dir:
+            logger.debug(f"Создаю дирректорию {directory}")
+            os.makedirs(directory)
+        if os.path.isfile(f"{directory}{output_file}"):
+            logger.debug(f"IN FILE: {directory}")
+            past_df = pd.read_csv(
+                'C:\\master_run\\data_history\\marketplaces\\wildberries\\sent_to\\2024-02-03\\df_final_stock.csv')
+            past_df["offer_id"] = past_df["offer_id"].astype(str)
+            if past_df.empty:
+                dataframe = dataframe.drop_duplicates(subset=['offer_id', 'warehouse_id']).reset_index(drop=True)
+                dataframe.to_csv(f"{directory}{output_file}", index=False)
+
+            else:
+                logger.debug(f"IN ELSE: {directory}")
+                result_update_df = pd.concat([past_df,
+                                              dataframe], axis=0).drop_duplicates(subset=['offer_id',
+                                                                                          'warehouse_id']).reset_index(
+                    drop=True)
+                result_update_df.to_csv(f"{directory}{output_file}", index=False)
+
+        elif not os.path.isfile(f"{directory}{output_file}"):
+            dataframe.to_csv(f"{directory}{output_file}", index=False)
 
 
-def character_padding(str_el_array):
-    '''in a text array, deletes the characters '-', '.', 
-       measures the length of each element and adds characters, making the elements the same length, 6 characters'''
-    for x in range(0, len(str_el_array)):
-        full = str_el_array[x].replace(' −', '').replace('.', '')
-        while len(full) < 6:
-            full += "0"
-        str_el_array.remove(str_el_array[x])
-        str_el_array.insert(x, full)
-    return str_el_array
-    # return data, values of the type '['011100', '011110', '011111', ...]'
-###################################################
+save_to_csv(dataframe, history_dir, output_file, result_dir='', is_only_history=True)
